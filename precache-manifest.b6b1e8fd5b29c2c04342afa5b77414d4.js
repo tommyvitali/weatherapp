8fdcde51590b78672b1437c6edee4ce9fb9fc015
TommasoVitali <tommyvitaly@gmail.com>
2020-07-20 16:22:42 +0200

    Updates

---
+++ precache-manifest.b6b1e8fd5b29c2c04342afa5b77414d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2addf03aeacc532665f51a958632b4",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e057645102c8d3259f5f",
+    "url": "/weatherapp/static/css/main.b3c64e0a.chunk.css"
+  },
+  {
+    "revision": "a0b6649004edfa3fe468",
+    "url": "/weatherapp/static/js/2.7bcb4cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.7bcb4cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e057645102c8d3259f5f",
+    "url": "/weatherapp/static/js/main.06142574.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "8a81f6f55b8c8155c14355d6fcad7c1c",
+    "url": "/weatherapp/static/media/logo2.8a81f6f5.png"
+  },
+  {
+    "revision": "c196e157c1e7240a48d89f0061f2145c",
+    "url": "/weatherapp/static/media/sfondo.c196e157.jpg"
+  },
+  {
+    "revision": "d754f01551ddb060f2a2ca39db16076d",
+    "url": "/weatherapp/static/media/sunny.d754f015.jpg"
+  }
+]);
